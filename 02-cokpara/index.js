899765fdb026b30fb0afd3d9f8641f8443152b64
Onur Simsek <onur.simsekk3@gmail.com>
2021-07-07 00:49:39 +0300

    Fix product counter func

--- 02-cokpara/index.js
+++ 02-cokpara/index.js
@@ -1,12 +1,11 @@
 const productList = [
-  { id: 1, name: 'Kola', price: 4, img: 'https://i.sozcu.com.tr/wp-content/uploads/2016/01/19/diyet-kola.jpg' },
-  { id: 2, name: 'Iskender', price: 30, img: 'https://i.lezzet.com.tr/images-xxlarge-recipe/ev-yapimi-iskender-33bd7089-fa36-4398-95f8-02c6463ea27c.jpg' },
-  { id: 3, name: 'Yat', price: 45005555559, img: 'https://i.ytimg.com/vi/9BCZpcgsAb8/maxresdefault.jpg'  },
-  { id: 4, name: 'Bahceli Ev', price: 95000000, img: 'https://www.neredekal.com/res/blog/1582812421_7.jpg' },
-  { id: 5, name: 'Araba Fabrikası', price: 1200000000555, img: 'https://i.ytimg.com/vi/rfMkp55oTv0/maxresdefault.jpg' },
-  { id: 6, name: 'Ada', price: 600000050000400, img: 'https://www.vladi-private-islands.de/fileadmin/_processed_/0/1/csm_little_whale_cay_007_821a0f921c.jpg' },
-  { id: 7, name: 'Saat', price: 1558, img: 'https://storage.googleapis.com/watchesguild/upload/staging/system/media_libraries/353/file.jpg' }
-  // ... Kendi örneklerinizi eklemeye çekinmeyin.
+  { id: 1, name: 'Kola', price: 4, count:1, img: 'https://i.sozcu.com.tr/wp-content/uploads/2016/01/19/diyet-kola.jpg' },
+  { id: 2, name: 'Iskender', price: 30, count:1, img: 'https://i.lezzet.com.tr/images-xxlarge-recipe/ev-yapimi-iskender-33bd7089-fa36-4398-95f8-02c6463ea27c.jpg' },
+  { id: 3, name: 'Yat', price: 45005555559, count:1, img: 'https://i.ytimg.com/vi/9BCZpcgsAb8/maxresdefault.jpg'  },
+  { id: 4, name: 'Bahceli Ev', price: 95000000, count:1, img: 'https://www.neredekal.com/res/blog/1582812421_7.jpg' },
+  { id: 5, name: 'Araba Fabrikası', price: 1200000000555, count:1, img: 'https://i.ytimg.com/vi/rfMkp55oTv0/maxresdefault.jpg' },
+  { id: 6, name: 'Ada', price: 600000050000400, count:1, img: 'https://www.vladi-private-islands.de/fileadmin/_processed_/0/1/csm_little_whale_cay_007_821a0f921c.jpg' },
+  { id: 7, name: 'Saat', price: 1558, count:1, img: 'https://storage.googleapis.com/watchesguild/upload/staging/system/media_libraries/353/file.jpg' }
 ];
 
 const productsBox = document.querySelector('#products');
@@ -22,7 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
         <button id="${product.id}" class="p-button">Sepete Ekle</button>
         <h4>${product.name}</h4>
         <span>Fiyatı: ${product.price} $</span>
-        <span class="class${product.id}">${counter}</span>
+        <span class="class${product.id} basket"></span>
     </div>
   `);
 });
@@ -56,13 +55,13 @@ productsBox.addEventListener('mouseover', (e) => {
 });
 
 // Product Counter - Sepet Function  
-let counter = 0;
-
 productsBox.addEventListener('click', (e) => {
   if (e.target.className === "p-button") {
     const basket = document.querySelector(`.class${e.target.id}`);
     
-    counter ++;
-    basket.innerHTML = counter;
+    const clickedProduct = productList.filter(el => el.id == e.target.id);
+    return (
+      basket.innerHTML = `${clickedProduct[0].count++} adet eklendi`
+    );
   };
 });
